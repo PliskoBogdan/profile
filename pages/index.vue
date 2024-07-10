df05db9739c00db8e0de185b64306f3357f4dee8
PliskoBogdan <fanatamas64@gmail.com>
2024-07-11 01:02:04 +0300

    feat: add base pages routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,3 @@
 <template>
-    <div class="main-container">
-        <div class="main-wrapper">
-
-            <!-- <section class="bg-green-default">section</section> -->
-            <Profile />
-            <aside class="bg-green-default">aside</aside>
-        </div>
-    </div>
+    <div>Hello</div>
 </template>
-
-<style lang="scss" scoped>
-.main-container  {
-    width: 1300px;
-}
-.main-wrapper {
-    display: grid;
-    grid-template-columns: 3fr 9fr;
-    gap: 1rem;
-}
-</style>
